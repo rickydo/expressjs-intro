d07ad83043183be4839c85e3748ba834ec0296a0
Phuc  Do <ricky.dop@gmail.com>
2018-02-09 11:51:18 -0600

    have routes working with get and post

--- index.js
+++ index.js
@@ -4,9 +4,21 @@ const app = express();
 // static file serving
 app.use(express.static('public'));
 
-// app.get('/', (req, res) => {
-//   res.send('Hello World!');
-// });
+// wild card asterisk allows dynamic requests
+// get | post | put | delete
+app.get('/blog/*.html', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.route('/form')
+  .get((res, req) => {
+    res.send('You would see a FORM');
+  })
+  .post((res, req) => {
+    res.send('This is the results');
+  })
+
+
 
 app.listen(3006, () => {
   console.log('We are running on port 3006');
