6b17ddd079e283b028226fdddb5f3b3b2be30309
Phuc  Do <ricky.dop@gmail.com>
2018-02-09 12:18:39 -0600

    utilizing middleware for data massage

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 // static file serving
 app.use(express.static('public'));
+
+// using middleware
+// app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true})); //extended allows us to send arrays and other data types
 
 // wild card asterisk allows dynamic requests
 // get | post | put | delete
@@ -17,7 +22,7 @@ app.route('/form')
     res.sendFile(__dirname + '/private/form.html');
   })
   .post((req, res) => {
-    res.send('This is the results');
+    res.send(req.body.name + ', This is the results');
   })
 
 
